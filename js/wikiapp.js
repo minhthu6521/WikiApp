4586e93ad6690de98d95b179345c408e4e7b72b0
minhthu6521 <minhthu6521@gmail.com>
2016-10-15 05:04:12 +0300

    Update wikiapp.js

--- js/wikiapp.js
+++ js/wikiapp.js
@@ -10,6 +10,7 @@ function searchText(searchStr){
         },
         success: function(obj){
             console.log(obj);
+            $("div.resultDiv").remove();
             for(i = 1; i < obj[1].length; i++){
                 var name = obj[1][i];
                 var description = obj[2][i];
